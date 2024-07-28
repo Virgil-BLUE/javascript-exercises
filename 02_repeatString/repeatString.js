75dc41b5038acb94398caca9c6664d64ae90bcad
Jordan <jordank.worley@gmail.com>
2024-07-28 01:40:05 -0400

    Completed exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
 
-};
+let a = ''
+
+for (i = 0; i < num; i++) {
+        a = a.concat(string)
+}
+
+if (num < 0) {
+    return('ERROR')
+} else {
+    return(a)
+}
+
+}   
 
 // Do not edit below this line
 module.exports = repeatString;
