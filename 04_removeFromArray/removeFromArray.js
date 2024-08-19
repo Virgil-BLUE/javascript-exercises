b029ed96ac99645615886a652042803687834ac9
Jordans Laptop <jordank.worley@gmail.com>
2024-08-19 05:56:25 -0400

    Update problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...arg) {
+
+let fixedArray = [];
+
+array.forEach((item) => {
+
+    if (!arg.includes(item)) {
+        fixedArray.push(item);
+    }
+}
+)
+
+return(fixedArray)
+
+}
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
-};
+let sum = a
+
+if (0 <= a && 0 <= b && Number.isInteger(a) && Number.isInteger(b)) {
+    if (a < b) {
+        do { 
+            a ++;
+            sum += a;
+    }
+        while (a < b)
+    }
+    else if (a > b) {
+        do { 
+            a --;
+            sum += a;
+    }
+        while (a > b)
+    }
+else (sum = "ERROR")
+}
+
+return(sum)
+
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
